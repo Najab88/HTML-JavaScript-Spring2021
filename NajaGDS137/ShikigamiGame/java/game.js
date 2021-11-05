28d183f399e21da6ca4a69941eb5cb18d4af4b04
Naja Brown <najabrown88@gmail.com>
2021-11-05 15:55:30 -0400

    ud

--- NajaGDS137/ShikigamiGame/java/game.js
+++ NajaGDS137/ShikigamiGame/java/game.js
@@ -10,32 +10,59 @@ var interval = 1000 / 60
 timer = setInterval(animate, interval)
 
 //images
-//var img = document.getElementById("gum")
-///var img2 = document.getElementById("gum2")
+var bgx =0
+var bgx2 =2048
+var bg = new Image() //sky background
+bg.src = "images/sky.jpg"
+bg.onload = function () {
+    animate()
+}
+var bg2 = new Image() //sky background
+bg.src = "images/sky.jpg"
+bg.onload = function () {
+    animate()
+}
+bg.vx = -1
+bg.vy = 0
+bg2.vx = -1
+bg2.vy = 0
+
+
+var ha = new Image() // player sprite
+ha.src = "images/haku.png"
+ha.onload = function () {
+    animate()
+}
+
+var cloud = new Image() // floating clouds
+cloud.src = "images/cloud.png"
+cloud.onload = function () {
+    animate()
+}
+
+var bul = new Image() //bullets
+bul.src = "images/gum2.png"
+bul.onload = function () {
+    animate()
+}
+
 
 //score = 0
 
 
 // this is the player
 var haku = new GameObject()
-haku.color = "rgb(230, 24, 144)"
-
-var shiki = new GameObject()
-shiki.color = "blue"
-
-var dirX = 0
-var dirY = 0
-
-var arena = new GameObject()
-arena.width = canvas.width
-arena.height = canvas.height
-
-//paddle position
+haku.color = "rgb(230, 24, 144, 0)"
 haku.x = 512
 haku.y = canvas.height / 2
-haku.width = 50
-haku.height = 50
+haku.width = 60
+haku.height = 80
+var rotate = 0
 
+
+// this is the enemy
+var shiki = new GameObject()
+shiki.color = "blue"
 shiki.x = 512
 shiki.y = 300
 shiki.width = 70
@@ -44,16 +71,40 @@ shiki.vx = 3
 shiki.vy = 0
 
 
+var dirX = 0
+var dirY = 0
+
+// this is the canvas size
+var arena = new GameObject()
+arena.width = canvas.width
+arena.height = canvas.height
+
 //bullets
+var shot = false
+var shooting = false
 var bullets = new GameObject()
 bullets.x = -900
 bullets.y = haku.y
 bullets.width = 25
 bullets.height = 25
-bullets.color = "red"
+bullets.color = "rgb(230, 24, 144, 0)"
 
-var shot = false
-var shooting = false
+
+//___________________________________this is the scrolling background__________________________________
+var amount = 8;
+var particles = [];
+var colors = ["white", "cyan", "grey"];
+
+// random particles
+for (var p = 0; p < amount; p++) {
+    particles[p] = new GameObject({ width: 10, height: 10 })
+    particles[p].x = Math.random() * canvas.height;
+    particles[p].y = Math.random() * canvas.width;
+    particles[p].vx = Math.random()*1+1
+    //particles[p].color = colors[Math.floor(random(0, 2.9))]
+    particles[p].Image = cloud;
+}
+//_______________________________________end scrolling bg________________________________________________
 
 
 
@@ -70,129 +121,149 @@ timer = setInterval(animate, interval)
 function animate() {
     //clear the screen
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    //rotate ++
 
+    bgx += bg.vx
+    bgx2 += bg2.vx
+    
+  
 
-
-    //keypresses
+    //__________________________________keypresses____________________________________________
     if (w) {
         //moves up 
         haku.y += -6
-
-
+        
     }
     if (s) {
         //moves down 
         haku.y += 6
-
-
     }
     if (a) {
         //moves left
         haku.x += -6
-
     }
     if (d) {
         //moves down 
         haku.x += 6
-
     }
+
+    //_______________________________________bulletpresses_____________________________________________
     if (ArrowUp && !shot) {
         //shoot up 
-
-
         shot = true
         reset(bullets)
         bullets.vy = dirY = -10
         bullets.vx = dirX = 0
-
     }
     if (ArrowDown && !shot) {
         //shoot down
-
-
         shot = true
         reset(bullets)
         bullets.vy = dirY = 10
         bullets.vx = dirX = 0
-
     }
     if (ArrowLeft && !shot) {
         //shoot left
-
         shot = true
         reset(bullets)
         bullets.vy = dirY = 0
         bullets.vx = dirX = -10
-
     }
     if (ArrowRight && !shot) {
         //shoot down 
-
         shot = true
         reset(bullets)
         bullets.vy = dirY = 0
         bullets.vx = dirX = 10
-
     }
 
+    //________________________________________bullets____________________________________________________________
 
     bullets.move()
-
-    /* //puts bullet back
-     if (bullets.y < 0 - bullets.height / 2) {
- 
-         reset(bullets)
-         bullets.x = -500
-     	
-     }
-        //bottom
-     if (bullets.y > canvas.height + bullets.height / 2) {
- 
-         reset(bullets)
-         bullets.y = -500
-         
-     }*/
 
     if (!arena.hitObject(bullets)) {
         reset(bullets)
         bullets.x = -500
-
-
         hak(haku)
     }
+    //bullet collisions
 
+    if (bullets.hitObject(shiki)) {
+
+        bullets.x = shiki.x
+        shiki.x = 3000
+    }
+    
+
+    ctx.drawImage(bg, bgx, 0, canvas.width, canvas.height)
+    ctx.drawImage(bg2, bgx2, 0, canvas.width, canvas.height)
+   
+    //_______________________________________this is the scrolling background____________________________________
+    for (var p = 0; p < particles.length; p++) {
+
+        particles[p].x += particles[p].vx;
+
+//random speed
+        particles[p].vx = random(1, 2.9)
+
+        //puts back on screen
+        if (particles[p].x > canvas.width) {
+            particles[p].x = 0
+        }
+
+        ctx.drawImage(cloud, particles[p].x, particles[p].y, 90, 90)
+        
+        
+        
+    }
+//puts back on screen
+if (bg.x > canvas.width) {
+    bg.x = 0
+} 
+    
+    //___________________________________________draw on screen__________________________________________
+    
+    
     bullets.drawRect()
     haku.drawRect()
     shiki.drawRect()
+    ctx.drawImage(bul, bullets.x-20, bullets.y -20, 50, 50)
+    ctx.drawImage(ha, haku.x -70,haku.y -78, 150, 150)
+    
     //score.drawScore()
 
 }
 
+//__________________________________________functions______________________________________________________
+function random(low, high) {
+    return Math.random() * (high - low) + low;
+}
 
-
-    // haku boundry
-    function hak(hak) {
-        if (hak.y > canvas.height - hak.height / 2) {
-            hak.y = canvas.height - hak.height / 2
-        }
-        if (hak.y < 0 + hak.height / 2) {
-            hak.y = 0 + hak.height / 2
-        }
-        if (hak.x > canvas.width - hak.width / 2) {
-            hak.x = canvas.width - hak.width / 2
-        }
-        if (hak.x < 0 + hak.width / 2) {
-            hak.x = 0 + hak.width / 2
-        }
+// haku boundry
+function hak(hak) {
+    if (hak.y > canvas.height - hak.height / 2) {
+        hak.y = canvas.height - hak.height / 2
     }
-     
-    
-
-    function reset(bullets) {
-        bullets.y = haku.y
-        bullets.x = haku.x
-
-        shot = false
-        bullets.vy = 0
+    if (hak.y < 0 + hak.height / 2) {
+        hak.y = 0 + hak.height / 2
     }
-   
+    if (hak.x > canvas.width - hak.width / 2) {
+        hak.x = canvas.width - hak.width / 2
+    }
+    if (hak.x < 0 + hak.width / 2) {
+        hak.x = 0 + hak.width / 2
+    }
+}
+
+
+
+function reset(bullets) {
+    bullets.y = haku.y
+    bullets.x = haku.x
+
+    shot = false
+    bullets.vy = 0
+}
+
+
+
--- NajaGDS137/ShikigamiGame/java/gameobject.js
+++ NajaGDS137/ShikigamiGame/java/gameobject.js
@@ -1,39 +1,28 @@
 //do not change this file!!!!
 
-function GameObject(x, y, w, h, color) {
+function GameObject(obj)
+{
 
-	//Default Values
-	if (x == undefined)
-		this.x = canvas.width / 2
-	else
-		this.x = x
-	if (y == undefined)
-		this.y = canvas.height / 2
-	else
-		this.y = y
+	this.x = canvas.width / 2;
+	this.y = canvas.height / 2;
+	this.width = 100;
+	this.height = 100;
+	this.color = "#ff0000";
+	this.force = 1;
+	this.ax = 1;
+	this.ay = 1;
+	this.vx = 0;
+	this.vy = 0;
 
-	if (w == undefined)
-		this.width = 100
-	else
-		this.width = w
-	if (h == undefined)
-		this.height = 100
-	else
-		this.height = h
+	if(obj!== undefined)
+		{
+			for(value in obj)
+			{
+				if(this[value]!== undefined)
+				this[value] = obj[value];
+			}
+		}
 
-	//player's color
-	if (color == undefined)
-		this.color = "pink"
-	else
-		this.color = color
-
-	//player's velocity or speed on each axis
-	this.vx = 0
-	this.vy = 0
-
-	
-
-	//This draws the player to the screen
 	this.drawRect = function () {
 		ctx.save()
 		ctx.fillStyle = this.color
@@ -44,49 +33,71 @@ function GameObject(x, y, w, h, color) {
 	}
 
 	this.drawCircle = function () {
-		ctx.save()
-		ctx.fillStyle = this.color
-		ctx.strokeStyle = "rgb(230, 24, 144)"
-		ctx.lineWidth = "5"
-		ctx.beginPath()
-		ctx.translate(this.x, this.y)
-		ctx.arc(0, 0, this.width / 2, 0, 360 * Math.PI / 180, true)
-		ctx.stroke()
-		ctx.closePath()
-		ctx.fill()
-		ctx.restore()
+		context.save();
+		context.fillStyle = this.color;
+		context.beginPath();
+		context.translate(this.x, this.y);
+		context.arc(0, 0, this.radius(), 0, 360 * Math.PI / 180, true);
+		context.closePath();
+		context.fill();
+		context.restore();
 
 	}
 
-	
-	//This changes the player's position
 	this.move = function () {
-		this.x += this.vx
-		this.y += this.vy
+		this.x += this.vx;
+		this.y += this.vy;
 	}
 
+
+	//---------Returns object's for the top, bottom, left and right of an object's bounding box.
 	this.left = function () {
-		return this.x - this.width / 2
+		return { x: this.x - this.width / 2, y: this.y }
 	}
 	this.right = function () {
-		return this.x + this.width / 2
+		return { x: this.x + this.width / 2, y: this.y }
 	}
 
 	this.top = function () {
-		return this.y - this.height / 2
+		return { x: this.x, y: this.y - this.height / 2 }
 	}
 	this.bottom = function () {
-		return this.y + this.height / 2
+		return { x: this.x, y: this.y + this.height / 2 }
 	}
 
 	this.hitObject = function (obj) {
-		if (this.left() < obj.right() &&
-			this.right() > obj.left() &&
-			this.top() < obj.bottom() &&
-			this.bottom() > obj.top()) {
+		if (this.left().x <= obj.right().x &&
+			this.right().x >= obj.left().x &&
+			this.top().y <= obj.bottom().y &&
+			this.bottom().y >= obj.top().y) {
 			return true
 		}
-		return false
+		return false;
 	}
 
+	//------Tests whether a single point overlaps the bounding box of another object-------
+	this.hitTestPoint = function(obj)
+	{
+		if(obj.x >= this.left().x && 
+		   obj.x <= this.right().x &&
+		   obj.y >= this.top().y &&  
+		   obj.y <= this.bottom().y)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	/*-----Sets or gets the radius value--------*/
+	this.radius = function(newRadius)
+	{
+		 if(newRadius==undefined)
+		 {
+			return this.width/2; 
+		 }
+		 else
+		 {
+			 return newRadius;
+		 }
+	}
 }
